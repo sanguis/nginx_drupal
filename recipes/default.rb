b97a7f7954a2e52e55c66809ac846626f147e463
Josh Beauregard <josh.beauregard@knectar.com>
2014-01-03 09:03:06 -0500

    creating directories

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,14 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#create apps directory
+remote_directory "/etc/nginx/apps" do
+  files_group "root"
+  files_owner "root"
+  files_mode 00644
+  owner "root"
+  group "root"
+  mode 00755
+end
+
 template "/etc/nginx/apps/drupal" do
   source "drupal.erb"
   action :create
 end
 
+#enable micro caching
 template "/etc/nginx/conf.d/micro.conf" do
   source "micro.conf.erb"
   action :create
+end
+
+# create vhost templates
+
+remote_directory "/etc/nginx/templates" do
+  files_group "root"
+  files_owner "root"
+  files_mode 00644
+  owner "root"
+  group "root"
+  mode 00755
 end
 
 template "/etc/nginx/templates/VHOST.conf" do
