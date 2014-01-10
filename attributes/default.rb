07d2eaa43aa77a4e8c28d6b9e64727e4159827a7
Josh Beauregard <josh.beauregard@knectar.com>
2014-01-10 14:01:35 -0500

    fixed atribute hash

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,18 +3,17 @@
 # Attributes:: default
 #
 
-default['innodb']['default-storage-engine']		        = 'innodb'
-default['innodb']['innodb_buffer_pool_size']		      = '300M'
-default['innodb']['innodb_log_file_size']		          = '10M'
-default['innodb']['/var/lib/mlkiysql/ib_logfile']     = '/var/lib/mlkiysql/ib_logfile*'
-default['innodb']['innodb_flush_method']		          = 'O_DIRECT'
-default['innodb']['innodb_file_per_table']		        = 1
-default['innodb']['innodb_flush_log_at_trx_commit']		= 2
-default['innodb']['innodb_log_buffer_size']		        = '1M'
-default['innodb']['innodb_additional_mem_pool_size']	= '20M'
-# num cpu's/cores x2 is a good base line for thread concuracy
-default['innodb']['innodb_thread_concurrency']		    = 8
-default['innodb']['innodb_open_files']		            = 1024
-default['innodb']['ignore-builtin-innodb']            = 'ON'
-default['innodb']['innodb_file_per_table']            = 'ON'
-default['innodb']['plugin-load']                      = TRUE
+default['nginx_drupal']['innodb']['default-storage-engine']		        = 'innodb'
+default['nginx_drupal']['innodb']['innodb_buffer_pool_size']		      = '300M'
+default['nginx_drupal']['innodb']['innodb_log_file_size']		          = '10M'
+default['nginx_drupal']['innodb']['/var/lib/mlkiysql/ib_logfile']     = '/var/lib/mlkiysql/ib_logfile*'
+default['nginx_drupal']['innodb']['innodb_flush_method']		          = 'O_DIRECT'
+default['nginx_drupal']['innodb']['innodb_file_per_table']		        = 1
+default['nginx_drupal']['innodb']['innodb_flush_log_at_trx_commit']		= 2
+default['nginx_drupal']['innodb']['innodb_log_buffer_size']		        = '1M'
+default['nginx_drupal']['innodb']['innodb_additional_mem_pool_size']	= '20M'
+default['nginx_drupal']['innodb']['innodb_thread_concurrency']		    = 8
+default['nginx_drupal']['innodb']['innodb_open_files']		            = 1024
+default['nginx_drupal']['innodb']['ignore-builtin-innodb']            = 'ON'
+default['nginx_drupal']['innodb']['innodb_file_per_table']            = 'ON'
+default['nginx_drupal']['innodb']['plugin-load']                      = TRUE
