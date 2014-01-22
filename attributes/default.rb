85866c26855f0e2ffc3e2cd2997429003bc868bc
Josh Beauregard <josh.beauregard@knectar.com>
2014-01-22 13:15:30 -0500

    fixed logfile setting

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 
 default['nginx_drupal']['innodb']['default-storage-engine']		        = 'innodb'
 default['nginx_drupal']['innodb']['innodb_log_file_size']		          = '10M'
-default['nginx_drupal']['innodb']['/var/lib/mlkiysql/ib_logfile']     = '/var/lib/mlkiysql/ib_logfile*'
+default['nginx_drupal']['innodb']['/var/lib/mlkiysql/ib_logfile']     = '/var/lib/mysql/ib_logfile*'
 default['nginx_drupal']['innodb']['innodb_log_buffer_size']		        = '1M'
 default['nginx_drupal']['innodb']['innodb_open_files']		            = 1024
 default['nginx_drupal']['innodb']['ignore-builtin-innodb']            = 'ON'
