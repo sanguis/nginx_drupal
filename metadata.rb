3dd9727fc09aeaf1c7c5784e43edf64962d60afa
Josh Beauregard <josh.beauregard@knectar.com>
2015-01-09 16:12:00 -0500

    updating lemp basic to work with mysql cookbook v 6

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'nginx_drupal'
 maintainer       'Josh Beauregard (sanguis)'
-maintainer_email 'sanguisdex@gmailcom'
+maintainer_email 'sanguisdex@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures nginx_drupal templates'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
 depends 'nginx'
-depends 'mysql'
+depends 'mysql', '~> 6.0'
 depends 'php'
 depends 'php-fpm'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#creating mysql server 
+
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'change me'
+  action [:create, :start]
+end
+
+mysql_config 'default' do
+  source 'mysite.cnf.erb'
+  notifies :restart, 'mysql_service[default]'
+  action :create
+end
+
+#install the mysql client
+
+mysql_client 'default' do
+  action :create
+end
+
 #create apps directory
 remote_directory "apps" do
   path "/etc/nginx/apps"
