04856a1541a1fcc9169a3b32acc2fa89e584f2ac
Josh Beauregard <josh.beauregard@knectar.com>
2015-08-03 13:33:15 -0400

    ADD uprading to take advantage of the new php fpm resource.
    no longer need the php-fpm cook book becase php includes that
    information

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: nginx_drupal
 # Recipe:: default
 #
-# Copyright (C) 2013 Knectar
+# Copyright (C) 2015 Knectar
 # 
 # All rights reserved - Do Not Redistribute
 #
@@ -27,59 +27,80 @@ end
 
 # create vhost templates
 
- remote_directory "templates" do
-   path "/etc/nginx/templates"
-   files_group "root"
-   files_owner "root"
-   files_mode 00644
-   owner "root"
-   group "root"
-   mode 00755
-   action :create_if_missing
- end
+remote_directory "templates" do
+  path "/etc/nginx/templates"
+  files_group "root"
+  files_owner "root"
+  files_mode 00644
+  owner "root"
+  group "root"
+  mode 00755
+  action :create_if_missing
+end
 
- template "/etc/nginx/templates/VHOST.conf" do
-   source "VHOST.conf.erb"
-   variables({
-     :private_dir => "sites/default/files/private"
-   })
-   action :create
- end
- #added inodb settings
- template "/etc/mysql/conf.d/innodb.cnf" do
-   source "innodb.cnf.erb"
-   action :create
- end
- 
+directory '/etc/nginx/snippets' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+
+  action :create
+end
+
+file 'password' do
+  path '/etc/nginx/snippets/password.conf'
+  backup 5
+  owner 'root'
+  group 'root'
+  mode '0644'
+  content 'content here'
+
+  action :create
+end
+
+template "/etc/nginx/templates/VHOST.conf" do
+  source "VHOST.conf.erb"
+  variables({
+    :private_dir => "sites/default/files/private"
+  })
+  action :create
+end
+#added inodb settings
+template "/etc/mysql/conf.d/innodb.cnf" do
+  source "innodb.cnf.erb"
+  action :create
+end
+
 
 ## install php libs via pear
 include_recipe 'php' 
 
 #APC and dependacies
-case node['platform_family']
-when 'rhel', 'fedora'
-  %w{ httpd-devel pcre pcre-devel }.each do |pkg|
-    package pkg do
-      action :install
+if default['php']['version'] > 5.5
+  case node['platform_family']
+  when 'rhel', 'fedora'
+    %w{ httpd-devel pcre pcre-devel }.each do |pkg|
+      package pkg do
+        action :install
+      end
     end
   end
-end
 
-php_pear "apc" do
-  action :install
-  directives(
-    :shm_segments=> node['nginx_drupal']['shm_segments'],
-    :shm_size => node['nginx_drupal']['shm_size '],
-    :ttl=> node['nginx_drupal']['ttl'],
-    :user_ttl=> node['nginx_drupal']['user_ttl'],
-    :enable_cli=> node['nginx_drupal']['enable_cli'],
-    :stat=> node['nginx_drupal']['stat'],
-    :stat_ctime=> node['nginx_drupal']['stat_ctime'],
-    :lazy_classes=> node['nginx_drupal']['lazy_classes'],
-    :lazy_functions=> node['nginx_drupal']['lazy_functions'],
-    :write_lock=> node['nginx_drupal']['write_lock'],
-    :rfc1867=> node['nginx_drupal']['rfc1867']
-  )
+  php_pear "apc" do
+    action :install
+    directives(
+      :shm_segments=> node['nginx_drupal']['shm_segments'],
+      :shm_size => node['nginx_drupal']['shm_size '],
+      :ttl=> node['nginx_drupal']['ttl'],
+      :user_ttl=> node['nginx_drupal']['user_ttl'],
+      :enable_cli=> node['nginx_drupal']['enable_cli'],
+      :stat=> node['nginx_drupal']['stat'],
+      :stat_ctime=> node['nginx_drupal']['stat_ctime'],
+      :lazy_classes=> node['nginx_drupal']['lazy_classes'],
+      :lazy_functions=> node['nginx_drupal']['lazy_functions'],
+      :write_lock=> node['nginx_drupal']['write_lock'],
+      :rfc1867=> node['nginx_drupal']['rfc1867']
+    )
+  end
 end
 
 =begin future needs
