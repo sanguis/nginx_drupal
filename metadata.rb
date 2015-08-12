8ef252ae0fc4ce890d70edfb055418532135cc2b
Josh Beauregard <josh.beauregard@knectar.com>
2015-08-12 13:26:46 -0400

    renamed drupal app template to use .conf extention

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'josh.beauregard@knectar.com'
 license          'All rights reserved'
 description      'Installs/Configures nginx_drupal templates'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.3'
+version          '1.0.0'
 
 depends 'nginx'
 depends 'mysql', '~> 6.0'
