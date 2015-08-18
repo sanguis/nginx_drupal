052d6462433369fb720f1d2bd95eb268fc79eb60
Josh Beauregard <josh.beauregard@knectar.com>
2015-08-18 16:52:56 -0400

    vhost creation for LWRP done

--- libraries/provider_drupal_instance.rb
+++ libraries/provider_drupal_instance.rb
@@ -23,15 +23,46 @@ class Chef
       end
     end
     def shortname
-      return  URI.parse.host.primary_url.gsub(/^www\./, '').slice[0, 6]
+      return URI.parse.host.gsub(/^www\./, '').slice[0, 6]
     end
     def alias
       return "#{shortname}.#{new_resource.instance}"
+    end
+    def server_name
+      if new_resource.url.is_a?
+        s.array.new()
+        new_resource.url.each {|u| s[] = u}
+        return s.join(' ')
+      else
+        return new_resource.url.URI.parse.host
+      end
+    end
+    def is_ssl
+      if URI.parse.scheme == "https"
+        return true
+      else
+        return false
+      end
     end
     
     action :create do
       #todo: create file system
       #todo: create vhost file
+      template "/etc/nginx/sites-enabled/#{alais}.conf" do
+        source 'vhost.erb'
+        owner 'root'
+        group 'root'
+        mode '0644'
+        variables(
+          is_ssl: is_ssl,
+          url: server_name,
+          path: new_resource.path,
+          passwd: new_resource.passwd,
+          is_ssl: is_ssl,
+          private_dir: new_resource.private_dir
+        )
+        action :create
+      end
       #todo: create add ssl
       #todo: create database
       #todo: create drush alieas
