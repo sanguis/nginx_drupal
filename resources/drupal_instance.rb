6426f96421d7dc08dd69aab4434824224ac08f23
Josh Beauregard <josh.beauregard@knectar.com>
2015-08-19 12:03:48 -0400

    created resources file

---
+++ resources/drupal_instance.rb
@@ -0,0 +1,20 @@
+#
+# Author:: Josh Beauregard <josh.beauregard@knectar.com>
+# Cookbook Name:: nginx_drupal
+# Resource:: drupal_instance
+#
+
+default_action :create
+
+actions :create, :delete
+
+attribute :url, :kind_of => Array, :requred => true, :name_attribute => true
+attribute :instance, :kind_of => String, :default => 'production'
+attribute :ssl_crt, :kind_of => String
+attribute :ssl_key, :kind_of => String
+attribute :ssl_chain, :kind_of => String
+attribute :passwd, :kind_of => Array
+attribute :app_path, :kind_of => String, :required => true
+attribute :public_files, :kind_of => String , :default => 'sites/default/files'
+attribute :private_files, :kind_of => String, :default => 'sites/default/files/_private'
+attribute :db, :kind_of => Hash, :default => {'user' => '', 'db' => '', 'password' => '', 'host' => 'localhost', 'prefix' => ''}
