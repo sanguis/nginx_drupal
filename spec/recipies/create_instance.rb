5685e7345b3fdbc7bca0d14150d015aabd6b3e15
Josh Beauregard <josh.beauregard@knectar.com>
2015-09-14 08:57:32 -0400

    addeing unworking test recipe

---
+++ spec/recipies/create_instance.rb
@@ -0,0 +1,10 @@
+nginx_drupal_drupal_instance [ 'www.example.com', 'example.com' ]  do 
+  url [ 'www.example.com', 'example.com' ] 
+  #ssl_crt  
+  #ssl_key  
+  #ssl_chain  
+  passwd [ 'example:$apr1$CfB./bRK$WuLx9Zf89GzzVhNrYnql9.' ] 
+  #app_path  
+  #public_files 
+  #private_files 
+end
